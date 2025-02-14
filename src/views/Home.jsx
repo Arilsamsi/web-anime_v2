dae686980115c7324c2b8c51556190daf0ef9670
Arilsamsi <arilsamsi2@gmail.com>
2025-02-14 21:45:26 +0800

    ganti footer

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -174,7 +174,16 @@ function Home() {
       <footer className="bg-secondary py-8">
         <div className="container">
           <div className="text-center text-secondary-foreground">
-            <p className="text-sm">© 2025 AnimaPlay. All rights reserved.</p>
+            <p className="text-sm">
+              © 2025{" "}
+              <a
+                className="underline text-red-500 font-bold"
+                href="https://github.com/Arilsamsi"
+              >
+                AnimePlay
+              </a>
+              . All rights reserved.
+            </p>
           </div>
         </div>
       </footer>
